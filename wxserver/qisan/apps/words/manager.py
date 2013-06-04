20f074c608bb9a1c907e17f1a53080357556b014
yimiqisan <yimiqisan@gmail.com>
2013-06-04 17:48:33 +0800

    add wxpost

--- wxserver/qisan/apps/words/manager.py
+++ wxserver/qisan/apps/words/manager.py
@@ -9,9 +9,10 @@ Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 
 from __future__ import with_statement
 
-from flask import Flask, request, _app_ctx_stack
+from flask import Flask, abort, request, _app_ctx_stack
 from sqlite3 import dbapi2 as sqlite3
 import json
+from hashlib import sha1
 from lxml import etree
 from StringIO import StringIO
 from datetime import datetime, timedelta
@@ -29,30 +30,88 @@ app.config.from_object(__name__)
 
 
 ISO_DATE_FORMAT = '%Y-%m-%d'
+_sha1 = lambda x: sha1(x).hexdigest()
 YESTERDAY = (datetime.today() - timedelta(days=1)).date().isoformat()
-
+TOKEN = 'b61e7ad8c5194994903cd11be6160dc0'
 
 def init_db():
 	with app.app_context():
-	    db = get_db()
-	    with app.open_resource('schema.sql', mode='r') as f:
-	        db.cursor().executescript(f.read())
-	    db.commit()
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 def get_db():
 	top = _app_ctx_stack.top
 	if not hasattr(top, 'sqlite_db'):
-	    sqlite_db = sqlite3.connect(app.config['DATABASE'])
-	    sqlite_db.row_factory = sqlite3.Row
-	    top.sqlite_db = sqlite_db
+		sqlite_db = sqlite3.connect(app.config['DATABASE'])
+		sqlite_db.row_factory = sqlite3.Row
+		top.sqlite_db = sqlite_db
 	return top.sqlite_db
 
 @app.teardown_appcontext
 def close_db_connection(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
+	"""Closes the database again at the end of the request."""
+	top = _app_ctx_stack.top
+	if hasattr(top, 'sqlite_db'):
+		top.sqlite_db.close()
+
+def check_xml_wf(xml):
+	""" 使用lxml.etree.parse 检测xml是否符合语法规范"""
+	# 参数xml是经过StringIO处理过的instance类型
+	try:
+		xml = etree.parse(xml)
+		return xml
+	except etree.XMLSyntaxError, e:
+		return False
+
+def get_message_data(xml):
+	"""解析xml 提取其中数据 并存入一个dict"""
+	data_dict = {}
+	for node in xml.iter():
+		data_dict[node.tag] = node.text
+	return data_dict
+
+
+def _check_signature(message):
+	signature = message.pop('signature')
+	keylist = list(message)
+	keylist.sort()
+	compare = ''.join([str(message[key]) for key in keylist])
+	other_keylist = ['timestamp', 'nonce', 'token']
+	other_compare = ''.join([str(message[key]) for key in other_keylist])
+	return signature == _sha1(compare) or signature == _sha1(other_compare)
+
+@app.route('/wx', methods=['GET'])
+def wx_get():
+	message = request.args.to_dict()
+	echostr = message.pop('echostr', 'error')
+	message['token'] = TOKEN
+	if not _check_signature(message):
+		abort(404)
+	return echostr
+
+
+@app.route('/wx', methods=['POST'])
+def wx_post():
+	result = ''
+	postStr = request.data
+	xml = StringIO(postStr)
+	xml_str = check_xml_wf(xml)
+	if xml_str is False:
+		return False
+	message_dict = get_message_data(xml_str)
+	try:
+		to_user_name = message_dict['ToUserName']
+		from_user_name = message_dict['FromUserName']
+		content = message_dict.get('Content', '').strip()
+		event = message_dict.get('Event', '').strip()
+		event_key = message_dict.get('EventKey', '').strip()
+		message_type = message_dict['MsgType']
+	except KeyError, e:
+		return result
+	return '你好'
+
 
 @app.route('/')
 @app.route('/<dtime>')
@@ -76,44 +135,6 @@ def create():
 	except:
 		return 'err'
 	return 'ok'
-
-def check_xml_wf(xml):
-    """ 使用lxml.etree.parse 检测xml是否符合语法规范"""
-    # 参数xml是经过StringIO处理过的instance类型
-    try:
-        xml = etree.parse(xml)
-        return xml
-    except etree.XMLSyntaxError, e:
-        return False
-
-def get_message_data(xml):
-    """解析xml 提取其中数据 并存入一个dict"""
-    data_dict = {}
-
-    for node in xml.iter():
-        data_dict[node.tag] = node.text
-    return data_dict
-
-@app.route('/wx', methods=['POST'])
-def wx_post():
-    postStr = request.data
-    xml = StringIO(postStr)
-    check_xml = check_xml_wf(xml)
-    if check_xml:
-        return check_xml
-    else:
-        return False
-    print message_dict
-    try:
-        to_user_name = message_dict['ToUserName']
-        from_user_name = message_dict['FromUserName']
-        content = message_dict.get('Content', '').strip()
-        event = message_dict.get('Event', '').strip()
-        event_key = message_dict.get('EventKey', '').strip()
-        message_type = message_dict['MsgType']
-    except KeyError, e:
-        return result
-    return '你好'
 
 
 if __name__ == '__main__':
